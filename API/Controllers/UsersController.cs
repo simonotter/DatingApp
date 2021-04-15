f988982dffcd01a242e3291750db3770e71b382f
Simon Otter <simonotter@gmail.com>
2021-04-15 13:07:36 +0100

    Added paging, sorting and filtering of members

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -33,9 +34,21 @@ namespace API.Controllers
 
     // api/users
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
     {
-      var users = await _userRepository.GetMembersAsync();
+      var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+      userParams.CurrentUsername = user.UserName;
+
+      if (string.IsNullOrEmpty(userParams.Gender))
+      {
+        userParams.Gender = user.Gender == "male" ? "female" : "male";
+      }
+
+      var users = await _userRepository.GetMembersAsync(userParams);
+
+      Response.AddPaginationHeader(users.CurrentPage, users.PageSize, 
+        users.TotalCount, users.TotalPages);
+
       return Ok(users);
     }
 
@@ -67,7 +80,7 @@ namespace API.Controllers
 
       var result = await _photoService.AddPhotoAsync(file);
 
-      if (result.Error !=null) return BadRequest(result.Error.Message);
+      if (result.Error != null) return BadRequest(result.Error.Message);
 
       var photo = new Photo
       {
@@ -84,7 +97,7 @@ namespace API.Controllers
 
       if (await _userRepository.SaveAllAsync())
       {
-        return CreatedAtRoute("GetUser", new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+        return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<PhotoDto>(photo));
       }
 
       return BadRequest("Problem adding photo.");
@@ -100,7 +113,7 @@ namespace API.Controllers
       if (photo.IsMain) return BadRequest("This is already your main photo.");
 
       var currentMain = user.Photos.FirstOrDefault(x => x.IsMain);
-      if (currentMain !=null) currentMain.IsMain = false;
+      if (currentMain != null) currentMain.IsMain = false;
       photo.IsMain = true;
 
       if (await _userRepository.SaveAllAsync()) return NoContent();
@@ -122,7 +135,7 @@ namespace API.Controllers
       if (photo.PublicId != null)
       {
         var result = await _photoService.DeletePhotoAsync(photo.PublicId);
-        if (result.Error != null) return BadRequest(result.Error.Message); 
+        if (result.Error != null) return BadRequest(result.Error.Message);
       }
 
       user.Photos.Remove(photo);
--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -10,58 +12,74 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-  public class UserRepository : IUserRepository
-  {
-    private readonly DataContext _context;
-    private readonly IMapper _mapper;
-    public UserRepository(DataContext context, IMapper mapper)
+    public class UserRepository : IUserRepository
     {
-      _mapper = mapper;
-      _context = context;
-    }
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+        public UserRepository(DataContext context, IMapper mapper)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
 
-    public async Task<MemberDto> GetMemberAsync(string username)
-    {
-      return await _context.Users
-        .Where(x => x.UserName == username)
-        .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-        .SingleOrDefaultAsync();
-    }
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+            return await _context.Users
+              .Where(x => x.UserName == username)
+              .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+              .SingleOrDefaultAsync();
+        }
 
-    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
-    {
-      return await _context.Users
-        .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
-        .ToListAsync();
-    }
+        public async Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams)
+        {
+            var query = _context.Users.AsQueryable();
 
-    public async Task<AppUser> GetUserByIdAsync(int id)
-    {
-      return await _context.Users.FindAsync(id);
-    }
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
 
-    public async Task<AppUser> GetUserByUsernameAsync(string username)
-    {
-      return await _context.Users
-        .Include(p => p.Photos)
-        .SingleOrDefaultAsync(x => x.UserName == username);
-    }
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
 
-    public async Task<IEnumerable<AppUser>> GetUsersAsync()
-    {
-      return await _context.Users
-        .Include(p => p.Photos)
-        .ToListAsync();
-    }
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
 
-    public async Task<bool> SaveAllAsync()
-    {
-      return await _context.SaveChangesAsync() > 0;
-    }
+            query = userParams.OrderBy switch
+            {
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
 
-    public void Update(AppUser user)
-    {
-      _context.Entry(user).State = EntityState.Modified;
+            return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper
+              .ConfigurationProvider).AsNoTracking(),
+                userParams.PageNumber, userParams.PageSize);
+        }
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<AppUser> GetUserByUsernameAsync(string username)
+        {
+            return await _context.Users
+              .Include(p => p.Photos)
+              .SingleOrDefaultAsync(x => x.UserName == username);
+        }
+
+        public async Task<IEnumerable<AppUser>> GetUsersAsync()
+        {
+            return await _context.Users
+              .Include(p => p.Photos)
+              .ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
     }
-  }
 }
