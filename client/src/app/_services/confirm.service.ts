80142a67756ebfbb55a04fe37f814046536c2ef9
Simon Otter <simonotter@gmail.com>
2021-05-02 16:34:08 +0100

    Moved to Unit of Work and optimised queries

---
+++ client/src/app/_services/confirm.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfirmService {
+  bsModalref: BsModalRef;
+
+  constructor(private modalService: BsModalService) {}
+
+  confirm(
+    title = 'Confirmation',
+    message = 'Are you sure you want to do this?',
+    btnOkText = 'Ok',
+    btnCancelText = 'Cancel'
+): Observable<boolean> {
+    const config = {
+      initialState: {
+        title,
+        message,
+        btnOkText,
+        btnCancelText
+      }
+    }
+    this.bsModalref = this.modalService.show(ConfirmDialogComponent, config);
+    
+    return new Observable<boolean>(this.getResult());
+  }
+
+  private getResult() {
+    return (observer) => {
+      const subscription = this.bsModalref.onHidden.subscribe(() => {
+        observer.next(this.bsModalref.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
+  }
+}
